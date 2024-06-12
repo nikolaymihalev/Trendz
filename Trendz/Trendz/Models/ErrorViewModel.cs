b27c33266179ac1f618cadb1eacca971bd1d549e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-12 16:01:46 +0000

    Created project

---
+++ Trendz/Trendz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Trendz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
