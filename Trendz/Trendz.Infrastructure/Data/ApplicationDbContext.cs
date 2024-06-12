ae9aab9278666686d447ee19d5345a1165b7b37b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-12 16:09:44 +0000

    Moved db context to infrastructure layer

--- Trendz/Trendz.Infrastructure/Data/ApplicationDbContext.cs
+++ Trendz/Trendz.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Trendz.Data
+namespace Trendz.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Trendz/Trendz/Program.cs
+++ Trendz/Trendz/Program.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Trendz.Data;
+using Trendz.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -16,7 +15,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,7 +22,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
