319eb42b526cfd6583b2e62b2c0a464efe1e9fba
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-13 14:40:23 +0000

    Created Category entity

---
+++ Trendz/Trendz.Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Trendz.Infrastructure.Data.Models
+{
+    [Comment("Category entity")]
+    public class Category
+    {
+        [Comment("Category identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Category name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Comment("Date the category was added")]
+        [Required]
+        public DateTime DateAdded { get; set; }
+    }
+}
