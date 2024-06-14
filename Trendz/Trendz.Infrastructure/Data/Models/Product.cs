8bdc03de9a3db34b601362bbcba061774d368e71
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-14 09:30:53 +0000

    Created Wishlist Item entity

--- Trendz/Trendz.Infrastructure/Data/Models/Product.cs
+++ Trendz/Trendz.Infrastructure/Data/Models/Product.cs
@@ -50,5 +50,6 @@ namespace Trendz.Infrastructure.Data.Models
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
         public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
         public ICollection<Review> Reviews { get; set; } = new List<Review>();
+        public ICollection<WishlistItem> WishlistItems { get; set; } = new List<WishlistItem>();
     }
 }
---
+++ Trendz/Trendz.Infrastructure/Data/Models/WishlistItem.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trendz.Infrastructure.Data.Models
+{
+    [Comment("Wishlist Item entity")]
+    public class WishlistItem
+    {
+        [Comment("Item identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Wishlist identifier")]
+        [Required]
+        public int WishlistId { get; set; }
+
+        [Comment("Product identifier")]
+        public int ProductId { get; set; }
+
+        [Comment("Date the item was added")]
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [ForeignKey(nameof(WishlistId))]
+        public Wishlist Wishlist { get; set; } = null!;
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+    }
+}
