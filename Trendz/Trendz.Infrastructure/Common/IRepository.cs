7ee7687e0e0f93441d160200050a20fc2c329bcc
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-16 12:34:29 +0000

    Created Repository interface and class

---
+++ Trendz/Trendz.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Trendz.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> AllReadonly<T>() where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+        Task DeleteAsync<T>(object id) where T : class;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Trendz/Trendz.Infrastructure/Common/Repository.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Trendz.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        public Task AddAsync<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> AllReadonly<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
