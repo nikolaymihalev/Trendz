a939004d41a5a3bed5475f2e159e8a8e7f1a916e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-21 13:53:56 +0000

    Created class for static error messages

---
+++ Trendz/Trendz.Infrastructure/Constants/ErrorMessageConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Trendz.Infrastructure.Constants
+{
+    public static class ErrorMessageConstants
+    {
+        public const string RequireErrorMessage = "The field {0} is required!";
+        public const string StringLengthErrorMessage = "The field {0} is between {2} and {1} characters long!";
+    }
+}
