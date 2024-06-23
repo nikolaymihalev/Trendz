7dcd5f1da9f2d0e716339bbe0fb46b1765fd6f12
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-23 11:05:38 +0000

    Created Cart model

---
+++ Trendz/Trendz.Core/Models/Cart/CartModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Trendz.Core.Models.Cart
+{
+    /// <summary>
+    /// Model for User cart
+    /// </summary>
+    public class CartModel
+    {
+        /// <summary>
+        /// Cart identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User identifier
+        /// </summary>
+        public string UserId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Date the cart was created
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+    }
+}
