c4b607c9884566a65985c14c033cca8d4f6804de
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-23 11:01:45 +0000

    Created and registered Cart service

--- Trendz/Trendz/Extensions/ServiceCollectionExtension.cs
+++ Trendz/Trendz/Extensions/ServiceCollectionExtension.cs
@@ -12,6 +12,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddApplicationServices(this IServiceCollection services) 
         {
             services.AddScoped<IBrandService, BrandService>();
+            services.AddScoped<ICartService, CartService>();
 
             return services;
         }
