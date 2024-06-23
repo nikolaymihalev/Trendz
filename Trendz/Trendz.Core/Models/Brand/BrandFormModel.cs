7d741f5f8750a03bfba602fd9a52ec45e4aa9236
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-23 10:50:00 +0000

    Created Brand models

---
+++ Trendz/Trendz.Core/Models/Brand/BrandFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Brand
+{
+    /// <summary>
+    /// Model for creating or editing Brand
+    /// </summary>
+    public class BrandFormModel
+    {
+        /// <summary>
+        /// Brand name
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.NameMaxLength,
+            MinimumLength = ValidationConstants.NameMinLength,
+            ErrorMessage = ErrorMessageConstants.StringLengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Trendz/Trendz.Core/Models/Brand/BrandInfoModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Trendz.Core.Models.Brand
+{
+    /// <summary>
+    /// Model for Brand information
+    /// </summary>
+    public class BrandInfoModel
+    {
+        /// <summary>
+        /// Brand identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Brand name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Date the brand was added
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+    }
+}
