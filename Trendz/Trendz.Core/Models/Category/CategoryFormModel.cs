1a92702b5e22aed2aaf4714f0fe27c99451fa10d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-24 08:09:51 +0000

    Created Category models

---
+++ Trendz/Trendz.Core/Models/Category/CategoryFormModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Category
+{
+    /// <summary>
+    /// Model for adding or editting Category
+    /// </summary>
+    public class CategoryFormModel
+    {
+        /// <summary>
+        /// Category identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Category name
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.NameMaxLength,
+            MinimumLength = ValidationConstants.NameMinLength,
+            ErrorMessage = ErrorMessageConstants.StringLengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Trendz/Trendz.Core/Models/Category/CategoryInfoModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Trendz.Core.Models.Category
+{
+    /// <summary>
+    /// Model for Category information 
+    /// </summary>
+    public class CategoryInfoModel
+    {
+        /// <summary>
+        /// Category identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Category name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Date the category was added
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+    }
+}
