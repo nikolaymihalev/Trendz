d63494742a093ed671358e15101efc04d0c5b806
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-24 08:23:10 +0000

    Created Cart Item model

---
+++ Trendz/Trendz.Core/Models/Cart/CartItemModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Cart
+{
+    /// <summary>
+    /// Model for Cart item
+    /// </summary>
+    public class CartItemModel
+    {
+        /// <summary>
+        /// Item identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Cart identifier
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public int CartId { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Item quantity
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Date the item was added
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+    }
+}
