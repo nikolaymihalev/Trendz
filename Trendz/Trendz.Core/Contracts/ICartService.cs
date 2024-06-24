7c120bf6f3e5708cde0b66874a39b2b0b2d27e4a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-24 08:01:15 +0000

    Implemented Cart service methods

--- Trendz/Trendz.Core/Contracts/ICartService.cs
+++ Trendz/Trendz.Core/Contracts/ICartService.cs
@@ -1,6 +1,12 @@
-﻿namespace Trendz.Core.Contracts
+﻿using Trendz.Core.Models.Cart;
+
+namespace Trendz.Core.Contracts
 {
     public interface ICartService
     {
+        Task<IEnumerable<CartModel>> GetAllCartsAsync();
+        Task AddAsync(CartModel model);
+        Task DeleteAsync(int id);
+        Task<CartModel> GetByIdAsync(int id);
     }
 }
--- Trendz/Trendz.Core/Services/CartService.cs
+++ Trendz/Trendz.Core/Services/CartService.cs
@@ -1,8 +1,75 @@
-﻿using Trendz.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using Trendz.Core.Contracts;
+using Trendz.Core.Models.Brand;
+using Trendz.Core.Models.Cart;
+using Trendz.Infrastructure.Common;
+using Trendz.Infrastructure.Constants;
+using Trendz.Infrastructure.Data.Models;
 
 namespace Trendz.Core.Services
 {
     public class CartService : ICartService
     {
+        private readonly IRepository repository;
+
+        public CartService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task AddAsync(CartModel model)
+        {
+            var entity = new Cart()
+            {
+                UserId = model.UserId,
+                DateCreated = DateTime.Now,
+            };
+
+            try
+            {
+                await repository.AddAsync<Cart>(entity);
+                await repository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException(ErrorMessageConstants.InvalidModelErrorMessage);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await repository.GetByIdAsync<Cart>(id);
+
+            if (entity != null)
+                await repository.DeleteAsync<Cart>(id);
+        }
+
+        public async Task<IEnumerable<CartModel>> GetAllCartsAsync()
+        {
+            return await repository.AllReadonly<Cart>()
+                .AsNoTracking()
+                .Select(x => new CartModel()
+                {
+                    Id = x.Id,
+                    UserId = x.UserId,
+                    DateCreated = x.DateCreated,
+                })
+                .ToListAsync();
+        }
+
+        public async Task<CartModel> GetByIdAsync(int id)
+        {
+            var entity = await repository.GetByIdAsync<Cart>(id);
+
+            if (entity == null)
+                throw new ArgumentNullException(ErrorMessageConstants.DoesntExistErrorMessage);
+
+            return new CartModel()
+            {
+                Id = entity.Id,
+                UserId = entity.UserId,
+                DateCreated = entity.DateCreated,
+            };
+        }
     }
 }
