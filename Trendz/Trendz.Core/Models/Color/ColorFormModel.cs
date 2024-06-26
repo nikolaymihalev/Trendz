e0ad6bf60373dda2993f93bd988bb5e9ff8f6bd3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-26 13:55:45 +0000

    Created Color models

---
+++ Trendz/Trendz.Core/Models/Color/ColorFormModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Color
+{
+    /// <summary>
+    /// Model for adding or editting Color
+    /// </summary>
+    public class ColorFormModel
+    {
+        /// <summary>
+        /// Color identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Color name
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Color hexvalue
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public string HexValue { get; set; } = string.Empty;
+    }
+}
---
+++ Trendz/Trendz.Core/Models/Color/ColorInfoModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Trendz.Core.Models.Color
+{
+    /// <summary>
+    /// Model for Color information
+    /// </summary>
+    public class ColorInfoModel
+    {
+        /// <summary>
+        /// Color identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Color name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Color hexvalue
+        /// </summary>
+        public string HexValue { get; set; } = string.Empty;
+    }
+}
