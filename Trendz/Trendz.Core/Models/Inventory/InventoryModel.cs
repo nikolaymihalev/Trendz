687dcf6752750ff9ae6b8cbc22b9215946e828fb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-01 16:21:45 +0000

    Created Inventory model

---
+++ Trendz/Trendz.Core/Models/Inventory/InventoryModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Inventory
+{
+    /// <summary>
+    /// Model for Inventory
+    /// </summary>
+    public class InventoryModel
+    {
+        /// <summary>
+        /// Inventory identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Product quantity in stock
+        /// </summary>
+        public int QuantityInStock { get; set; }
+
+        /// <summary>
+        /// Inventory warehouse location
+        /// </summary>
+        public string WarehouseLocation { get; set; } = string.Empty;
+    }
+}
