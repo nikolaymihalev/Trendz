66333b9537332e7410797f517b818b29c555cbb4
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-01 16:33:24 +0000

    Created Order info model

---
+++ Trendz/Trendz.Core/Models/Order/OrderInfoModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Trendz.Core.Models.Order
+{
+    /// <summary>
+    /// Model for Order information
+    /// </summary>
+    public class OrderInfoModel
+    {
+        /// <summary>
+        /// Order identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User identifier
+        /// </summary>        
+        public string UserId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Order date
+        /// </summary>
+        public DateTime OrderDate { get; set; }
+
+        /// <summary>
+        /// Shipping date
+        /// </summary>
+        public DateTime ShippingDate { get; set; }
+
+        /// <summary>
+        /// Order status
+        /// </summary>
+        public string Status { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Total amount of order
+        /// </summary>
+        public decimal TotalAmount { get; set; }        
+    }
+}
