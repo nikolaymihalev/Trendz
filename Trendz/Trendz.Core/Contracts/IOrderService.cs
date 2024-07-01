4aa04b0952d20b9ef0727640f6d3de3eb04b935d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-01 16:42:20 +0000

    Implemented Order service methods

--- Trendz/Trendz.Core/Contracts/IOrderService.cs
+++ Trendz/Trendz.Core/Contracts/IOrderService.cs
@@ -1,6 +1,13 @@
-﻿namespace Trendz.Core.Contracts
+﻿using Trendz.Core.Models.Order;
+
+namespace Trendz.Core.Contracts
 {
     public interface IOrderService
     {
+        Task<IEnumerable<OrderInfoModel>> GetAllOrdersAsync();
+        Task AddAsync(OrderFormModel model);
+        Task EditAsync(OrderFormModel model);
+        Task DeleteAsync(int id);
+        Task<OrderInfoModel> GetByIdAsync(int id);
     }
 }
--- Trendz/Trendz.Core/Services/OrderService.cs
+++ Trendz/Trendz.Core/Services/OrderService.cs
@@ -1,8 +1,97 @@
-﻿using Trendz.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using Trendz.Core.Contracts;
+using Trendz.Core.Models.Order;
+using Trendz.Infrastructure.Common;
+using Trendz.Infrastructure.Constants;
+using Trendz.Infrastructure.Data.Models;
 
 namespace Trendz.Core.Services
 {
     public class OrderService : IOrderService
     {
+        private readonly IRepository repository;
+
+        public OrderService(IRepository _repositor)
+        {
+            repository = _repositor;
+        }
+
+        public async Task AddAsync(OrderFormModel model)
+        {
+            var entity = new Order()
+            {
+                UserId = model.UserId,
+                OrderDate = model.OrderDate,
+                ShippingDate = model.ShippingDate,
+                Status = model.Status,
+                TotalAmount = model.TotalAmount,
+            };
+
+            try
+            {
+                await repository.AddAsync<Order>(entity);
+                await repository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException(ErrorMessageConstants.InvalidModelErrorMessage);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await repository.GetByIdAsync<Order>(id);
+
+            if (entity != null)
+                await repository.DeleteAsync<Order>(id);
+        }
+
+        public async Task EditAsync(OrderFormModel model)
+        {
+            var entity = await repository.GetByIdAsync<Order>(model.Id);
+
+            if (entity != null)
+            {
+                entity.OrderDate = model.OrderDate;
+                entity.ShippingDate = model.ShippingDate;
+                entity.Status = model.Status;
+                entity.TotalAmount = model.TotalAmount;
+
+                await repository.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<OrderInfoModel>> GetAllOrdersAsync()
+        {
+            return await repository.AllReadonly<Order>()
+                .Select(x => new OrderInfoModel()
+                {
+                    Id = x.Id,
+                    UserId = x.UserId,
+                    OrderDate = x.OrderDate,
+                    ShippingDate = x.ShippingDate,
+                    Status = x.Status,
+                    TotalAmount = x.TotalAmount,
+                })
+                .ToListAsync();
+        }
+
+        public async Task<OrderInfoModel> GetByIdAsync(int id)
+        {
+            var entity = await repository.GetByIdAsync<Order>(id);
+
+            if (entity == null)
+                throw new ArgumentNullException(ErrorMessageConstants.DoesntExistErrorMessage);
+
+            return new OrderInfoModel()
+            {
+                Id = entity.Id,
+                UserId = entity.UserId,
+                OrderDate = entity.OrderDate,
+                ShippingDate = entity.ShippingDate,
+                Status = entity.Status,
+                TotalAmount = entity.TotalAmount,
+            };
+        }
     }
 }
