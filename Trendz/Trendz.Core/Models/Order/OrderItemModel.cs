11a5f976de6546c7d110fb5b84469de80acbc919
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-01 16:44:23 +0000

    Created model for Order Item

---
+++ Trendz/Trendz.Core/Models/Order/OrderItemModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Order
+{
+    /// <summary>
+    /// Model for Order Item
+    /// </summary>
+    public class OrderItemModel
+    {
+        /// <summary>
+        /// Item identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Order identifier
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Item quantity
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Item unit price
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public decimal UnitPrice { get; set; }
+    }
+}
