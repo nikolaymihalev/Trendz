f3545fc5743a8aebbb66b1057044cda13c9d7304
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-03 06:42:48 +0000

    Created Payment info model

---
+++ Trendz/Trendz.Core/Models/Payment/PaymentInfoModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Trendz.Core.Models.Payment
+{
+    /// <summary>
+    /// Model for Payment information
+    /// </summary>
+    public class PaymentInfoModel
+    {
+        /// <summary>
+        /// Payment identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Order identifier
+        /// </summary>
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Payment date
+        /// </summary>
+        public DateTime PaymentDate { get; set; }
+
+        /// <summary>
+        /// Payment amount
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Payment method
+        /// </summary>
+        public string PaymentMethod { get; set; } = string.Empty;
+    }
+}
