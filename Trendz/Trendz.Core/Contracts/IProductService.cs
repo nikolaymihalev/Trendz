4a4e0e0040ab3164d4b5fc83d1e3b949610523fe
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-04 07:40:12 +0000

    Created interface for Product service

---
+++ Trendz/Trendz.Core/Contracts/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Trendz.Core.Models.Product;
+
+namespace Trendz.Core.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductInfoModel>> GetAllProductsAsync();
+        Task AddAsync(ProductFormModel model);
+        Task EditAsync(ProductFormModel model);
+        Task DeleteAsync(int id);
+        Task<ProductInfoModel> GetByIdAsync(int id);
+    }
+}
