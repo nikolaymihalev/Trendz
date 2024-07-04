bddbcf29f554741ce9b305832e539acee1310f5a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-04 07:51:16 +0000

    Created Product Color model

---
+++ Trendz/Trendz.Core/Models/Product/ProductColorModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Trendz.Core.Models.Product
+{
+    /// <summary>
+    /// Model for mapping Product and Color
+    /// </summary>
+    public class ProductColorModel
+    {
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Color identifier
+        /// </summary>
+        public int ColorId { get; set; }
+    }
+}
