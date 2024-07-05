71e07be37f7191cacb8984fe47a1c5da95d68240
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-05 06:37:22 +0000

    Created Product Image form model

---
+++ Trendz/Trendz.Core/Models/Product/ProductImageFormModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Product
+{
+    /// <summary>
+    /// Model for adding or editing Product Image
+    /// </summary>
+    public class ProductImageFormModel
+    {
+        /// <summary>
+        /// Image identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Image of the product
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public byte[] Image { get; set; } = new byte[128];
+
+        /// <summary>
+        /// Uploaded file for product image
+        /// </summary>
+        public IFormFile? ImageFile { get; set; }
+    }
+}
