b4e8d42e09ca19dc611a3357cedad92e848332f1
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-05 06:34:05 +0000

    Created Product Image info model

---
+++ Trendz/Trendz.Core/Models/Product/ProductImageInfoModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Trendz.Core.Models.Product
+{
+    /// <summary>
+    /// Model for Product Image information
+    /// </summary>
+    public class ProductImageInfoModel
+    {
+        /// <summary>
+        /// Image identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Image file
+        /// </summary>
+        public string Image { get; set; } = string.Empty;
+    }
+}
