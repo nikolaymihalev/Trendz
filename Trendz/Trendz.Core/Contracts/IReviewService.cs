520009705175f43162c49bb69f8339ab8cd26741
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2024-07-08 09:12:24 +0300

    Created interface for Review service

---
+++ Trendz/Trendz.Core/Contracts/IReviewService.cs
@@ -0,0 +1,14 @@
+﻿using Trendz.Core.Models.Review;
+
+namespace Trendz.Core.Contracts
+{
+    public interface IReviewService
+    {
+        Task<IEnumerable<ReviewInfoModel>> GetAllReviewsAsync();
+        Task<IEnumerable<ReviewInfoModel>> GetAllReviewsForProductAsync(int id);
+        Task AddAsync(ReviewFormModel model);
+        Task EditAsync(ReviewFormModel model);
+        Task DeleteAsync(int id);
+        Task<ReviewInfoModel> GetByIdAsync(int id);
+    }
+}
