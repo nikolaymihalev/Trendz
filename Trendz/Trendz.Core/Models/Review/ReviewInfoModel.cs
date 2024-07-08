540806231c361486f6eb8df2327cce0746728b8e
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2024-07-08 09:08:20 +0300

    Created Review info model

---
+++ Trendz/Trendz.Core/Models/Review/ReviewInfoModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Trendz.Core.Models.Review
+{
+    /// <summary>
+    /// Model for Review information
+    /// </summary>
+    public class ReviewInfoModel
+    {
+        /// <summary>
+        /// Review identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// User identifier
+        /// </summary>
+        public string UserId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Review rating
+        /// </summary>
+        public int Rating { get; set; }
+
+        /// <summary>
+        /// User comment
+        /// </summary>
+        public string Comment { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Date the review was published
+        /// </summary>
+        public DateTime PublishDate { get; set; }
+    }
+}
