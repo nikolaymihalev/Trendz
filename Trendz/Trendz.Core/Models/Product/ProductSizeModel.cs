03901c65e42119a6e5d4f1bd081b987dea3d43d3
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2024-07-08 08:56:58 +0300

    Created Product Size model

---
+++ Trendz/Trendz.Core/Models/Product/ProductSizeModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Trendz.Core.Models.Product
+{
+    /// <summary>
+    /// Model for mapping Product and Size
+    /// </summary>
+    public class ProductSizeModel
+    {
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Size identifier
+        /// </summary>
+        public int SizeId { get; set; }
+    }
+}
