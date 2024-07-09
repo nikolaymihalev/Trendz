50c9005f05ad78aaafb9f71527f24ac696e9a1e9
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2024-07-09 08:22:27 +0300

    Created Shipping info model

---
+++ Trendz/Trendz.Core/Models/Shipping/ShippingInfoModel.cs
@@ -0,0 +1,53 @@
+﻿namespace Trendz.Core.Models.Shipping
+{
+    /// <summary>
+    /// Model for Shipping information
+    /// </summary>
+    public class ShippingInfoModel
+    {
+        /// <summary>
+        /// Shipping identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Order identifier
+        /// </summary>
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Shipping address
+        /// </summary>
+        public string ShippingAddress { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The city from the address
+        /// </summary>
+        public string City { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The state from the address
+        /// </summary>
+        public string State { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The postal code from the city
+        /// </summary>
+        public string PostalCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The country from the address
+        /// </summary>
+        public string Country { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Shipping date
+        /// </summary>
+        public DateTime ShippingDate { get; set; }
+
+        /// <summary>
+        /// Delivery date
+        /// </summary>
+        public DateTime DeliveryDate { get; set; }
+    }
+}
