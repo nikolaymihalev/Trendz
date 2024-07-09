afe1a3c29433090fd46adbe9096f36ad21815c30
Nikolay Mihalev <122871592+nikolaymihalev@users.noreply.github.com>
2024-07-09 08:25:53 +0300

    Created interface for Shipping service

---
+++ Trendz/Trendz.Core/Contracts/IShippingService.cs
@@ -0,0 +1,14 @@
+﻿using Trendz.Core.Models.Shipping;
+
+namespace Trendz.Core.Contracts
+{
+    public interface IShippingService
+    {
+        Task<IEnumerable<ShippingInfoModel>> GetAllShippingsAsync();
+        Task<IEnumerable<ShippingInfoModel>> GetAllShippingsForUserAsync(string userId);
+        Task AddAsync(ShippingFormModel model);
+        Task EditAsync(ShippingFormModel model);
+        Task DeleteAsync(int id);
+        Task<ShippingInfoModel> GetByIdAsync(int id);
+    }
+}
