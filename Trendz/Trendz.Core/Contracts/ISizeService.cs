79f477f57828bb9cddfc5719a6db49e3faa22293
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-10 15:28:59 +0000

    Created interface for Size service

---
+++ Trendz/Trendz.Core/Contracts/ISizeService.cs
@@ -0,0 +1,13 @@
+﻿using Trendz.Core.Models.Size;
+
+namespace Trendz.Core.Contracts
+{
+    public interface ISizeService
+    {
+        Task<IEnumerable<SizeInfoModel>> GetAllColorsAsync();
+        Task AddAsync(SizeFormModel model);
+        Task EditAsync(SizeFormModel model);
+        Task DeleteAsync(int id);
+        Task<SizeInfoModel> GetByIdAsync(int id);
+    }
+}
