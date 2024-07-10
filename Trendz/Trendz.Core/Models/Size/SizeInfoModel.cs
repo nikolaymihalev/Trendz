c9ec481e23fe087526fb711c25d1fb89139c847b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-10 15:26:20 +0000

    Created Size info model

---
+++ Trendz/Trendz.Core/Models/Size/SizeInfoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Trendz.Core.Models.Size
+{
+    /// <summary>
+    /// Model for Size information
+    /// </summary>
+    public class SizeInfoModel
+    {
+        /// <summary>
+        /// Size identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Size name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+    }
+}
