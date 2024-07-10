1d92fd14933fdb7e106a55f22a72a0615922cd4b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-10 15:27:27 +0000

    Created Size form model

---
+++ Trendz/Trendz.Core/Models/Size/SizeFormModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.Size
+{
+    /// <summary>
+    /// Model for adding or editing Size
+    /// </summary>
+    public class SizeFormModel
+    {
+        /// <summary>
+        /// Size identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Size name
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
