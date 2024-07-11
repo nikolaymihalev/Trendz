be74937bec667d4e374965c7926688960de2e88d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-11 16:35:23 +0000

    Created Wishlist model

---
+++ Trendz/Trendz.Core/Models/Wishlist/WishlistModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Trendz.Core.Models.Wishlist
+{
+    /// <summary>
+    /// Model for Wishlist
+    /// </summary>
+    public class WishlistModel
+    {
+        /// <summary>
+        /// Wishlist identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User identifier
+        /// </summary>
+        public string UserId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Date the wishlist was created
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+    }
+}
