9d1e7a91fcd2095d34345549d81fe3b64fe5ac12
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-11 16:37:04 +0000

    Created interface for Wishlist service

---
+++ Trendz/Trendz.Core/Contracts/IWishlistService.cs
@@ -0,0 +1,13 @@
+﻿using Trendz.Core.Models.Wishlist;
+
+namespace Trendz.Core.Contracts
+{
+    public interface IWishlistService
+    {
+        Task<IEnumerable<WishlistModel>> GetAllWishlistsAsync();
+        Task AddAsync(WishlistModel model);
+        Task DeleteAsync(int id);
+        Task<WishlistModel> GetByIdAsync(int id);
+        Task<WishlistModel> GetByUserIdAsync(string userId);
+    }
+}
