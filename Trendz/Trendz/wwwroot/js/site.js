e3678ed0e50f04ebff2ad265dd0f5e33274bc0e2
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-07 13:28:05 +0000

    Added show hide function to search form label

--- Trendz/Trendz/wwwroot/js/site.js
+++ Trendz/Trendz/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿$('.nav-search form input').on('.nav-search form input', function () {
+    if ($(this).val() != '') {
+        $(this).parent().find('label').css('display','none');
+    } else {
+        $(this).parent().find('label').css('display', 'block');
+    }
+});
