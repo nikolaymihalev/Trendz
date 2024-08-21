db369ca68fb742abd60c57b37877b408722fae63
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-21 19:50:45 +0300

    Created interface for Rating Service

---
+++ Trendz/Trendz.Core/Contracts/IRatingService.cs
@@ -0,0 +1,11 @@
+﻿namespace Trendz.Core.Contracts
+{
+    public interface IRatingService
+    {
+        Task AddAsync(RatingFormModel model);
+        Task DeleteAsync(int id);
+        Task EditAsync(RatingFormModel model);
+        Task<double> GetAverageRatingAboutProductAsync(int productId);
+        Task<IEnumerable<RatingInfoModel>> GetAllRatingsAboutProductAsync(int productId);
+    }
+}
