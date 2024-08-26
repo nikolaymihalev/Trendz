4ff59d8ec7966c95c6ea7d310475ffcc9d3f0b07
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-26 20:40:19 +0300

    Create RegisterModel.cs

---
+++ Trendz/Trendz.Core/Models/User/RegisterModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.User
+{
+    /// <summary>
+    /// User profile model for register
+    /// </summary>
+    public class RegisterModel
+    {
+        /// <summary>
+        /// User email
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [EmailAddress]
+        [StringLength(ValidationConstants.EmailMaxLength,
+            MinimumLength = ValidationConstants.EmailMinLength,
+            ErrorMessage = ErrorMessageConstants.StringLengthErrorMessage)]
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// User password
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.PasswordMaxLength,
+            MinimumLength = ValidationConstants.PasswordMinLength,
+            ErrorMessage = ErrorMessageConstants.StringLengthErrorMessage)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
