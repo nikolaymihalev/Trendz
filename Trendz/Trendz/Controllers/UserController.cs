63811ed4e490466ecd56df5fcc9fcc33fe2a81ff
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-26 20:37:24 +0300

    Created User Controller

---
+++ Trendz/Trendz/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trendz.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        
+        public UserController(
+            UserManager<IdentityUser> _userManager,
+            SignInManager<IdentityUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+    }
+}
