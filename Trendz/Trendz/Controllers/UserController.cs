5ae1145e75e3368942b38e5014be853d1df06d9a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-26 20:44:30 +0300

    Implemented Register operation

--- Trendz/Trendz/Controllers/UserController.cs
+++ Trendz/Trendz/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Trendz.Core.Models.User;
 
 namespace Trendz.Controllers
 {
@@ -7,13 +9,51 @@ namespace Trendz.Controllers
     {
         private readonly UserManager<IdentityUser> userManager;
         private readonly SignInManager<IdentityUser> signInManager;
-        
+
         public UserController(
             UserManager<IdentityUser> _userManager,
             SignInManager<IdentityUser> _signInManager)
         {
             userManager = _userManager;
             signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Home", "Post");
+            }
+
+            var model = new RegisterModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+
+
+            var user = new IdentityUser()
+            {
+                Email = model.Email,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+            return RedirectToAction("Home", "Index");
         }
     }
 }
