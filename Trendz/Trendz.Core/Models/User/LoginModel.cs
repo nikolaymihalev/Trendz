66bfa78c651b33df983b255496387354b0dd268c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-26 20:39:35 +0300

    Create LoginModel.cs

---
+++ Trendz/Trendz.Core/Models/User/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trendz.Infrastructure.Constants;
+
+namespace Trendz.Core.Models.User
+{
+    /// <summary>
+    /// User profile model for login
+    /// </summary>
+    public class LoginModel
+    {
+        /// <summary>
+        /// User email
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// User password
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
