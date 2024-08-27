a37805cf7a91495a8587e9c17836e9029b7ae3c0
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-27 15:01:40 +0300

    Update HomeController.cs

--- Trendz/Trendz/Controllers/HomeController.cs
+++ Trendz/Trendz/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Trendz.Models;
 
 namespace Trendz.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -13,6 +14,7 @@ namespace Trendz.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
