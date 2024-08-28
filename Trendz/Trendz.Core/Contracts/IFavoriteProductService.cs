3337e5550c1085a9348f7d61b72207f97854e01d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-28 12:07:18 +0300

    Created interface for Favorite Product service

---
+++ Trendz/Trendz.Core/Contracts/IFavoriteProductService.cs
@@ -0,0 +1,13 @@
+﻿using Trendz.Core.Models.Product;
+
+namespace Trendz.Core.Contracts
+{
+    public interface IFavoriteProductService
+    {
+        Task AddAsync(FavoriteProductModel model);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<FavoriteProductModel>> GetUserFavoriteProductAsync(string userId);
+        Task<bool> IsFavoriteProductExistsAsync(int productId, string userId);
+        Task<FavoriteProductModel?> GetFavoriteProductAsync(int productId, string userId);
+    }
+}
