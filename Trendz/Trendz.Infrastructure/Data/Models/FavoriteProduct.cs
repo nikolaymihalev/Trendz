4a9778b5d448aec6554d7aeb79e6fb8b1355588f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-28 11:45:37 +0300

    Created Favorite Product

---
+++ Trendz/Trendz.Infrastructure/Data/Models/FavoriteProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trendz.Infrastructure.Data.Models
+{
+    [Comment("User favorite product")]
+    public class FavoriteProduct
+    {
+        [Comment("Favorite product identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Product identifier")]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Comment("User identifier")]
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+
+        [ForeignKey(nameof(ProductId))]
+        public IdentityUser User { get; set; } = null!;
+    }
+}
