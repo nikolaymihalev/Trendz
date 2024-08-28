60742402d55f6c76559f33d11531132de5401aef
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-28 12:05:12 +0300

    Created Favorite Product model

---
+++ Trendz/Trendz.Core/Models/Product/FavoriteProductModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Trendz.Core.Models.Product
+{
+    /// <summary>
+    /// Model for information about user favorite product
+    /// </summary>
+    public class FavoriteProductModel
+    {
+        /// <summary>
+        /// Favorite product identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public int ProcuctId { get; set; }
+
+        /// <summary>
+        /// User identifier
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Product model
+        /// </summary>
+        public ProductInfoModel Product { get; set; }
+    }
+}
