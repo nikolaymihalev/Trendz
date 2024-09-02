ac47cbd9b69c99e5e32e68990a162838a3eef57e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-09-02 13:16:10 +0300

    Create ProductQueryModel.cs

---
+++ Trendz/Trendz.Core/Models/Product/ProductQueryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Trendz.Core.Models.Product
+{
+    public class ProductQueryModel
+    {
+        public IEnumerable<ProductInfoModel> Products { get; set; } = new List<ProductInfoModel>();
+        public string? Category { get; set; }
+        public string? Sorting { get; set; }
+        public double PagesCount { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
