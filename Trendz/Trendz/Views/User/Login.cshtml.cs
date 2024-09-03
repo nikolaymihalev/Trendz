408429b92787294924da4ab7ac7cfe4ae51496bb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-09-03 14:40:56 +0300

    Fix scaffolded register view

--- Trendz/Trendz/Views/User/Login.cshtml.cs
+++ Trendz/Trendz/Views/User/Login.cshtml.cs
@@ -15,7 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace Trendz.Areas.Identity.Pages.Account
+namespace Trendz.Views.User
 {
     public class LoginModel : PageModel
     {
@@ -119,7 +119,7 @@ namespace Trendz.Areas.Identity.Pages.Account
                 }
                 if (result.RequiresTwoFactor)
                 {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, Input.RememberMe });
                 }
                 if (result.IsLockedOut)
                 {
--- Trendz/Trendz/Views/User/Logout.cshtml.cs
+++ Trendz/Trendz/Views/User/Logout.cshtml.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace Trendz.Areas.Identity.Pages.Account
+namespace Trendz.Views.User
 {
     public class LogoutModel : PageModel
     {
